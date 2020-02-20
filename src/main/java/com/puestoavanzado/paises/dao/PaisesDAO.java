d4b0c9b04253456f01d999a1b9c2f0156f118120
Agustin Paoletti <agustinpaoletti92@outlook.com>
2020-02-19 22:40:34 -0300

    Added Commentary

--- src/main/java/com/puestoavanzado/paises/dao/PaisesDAO.java
+++ src/main/java/com/puestoavanzado/paises/dao/PaisesDAO.java
@@ -5,6 +5,7 @@ import com.puestoavanzado.paises.entity.Paises;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//Usado para settear endpoint
 @RepositoryRestResource(path="countries")
 public interface PaisesDAO extends JpaRepository<Paises, Integer> {
     
